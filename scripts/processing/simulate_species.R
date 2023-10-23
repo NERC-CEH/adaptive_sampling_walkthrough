fd85cd3b32676d8508676e29d16cc29a68ef6fcf
Thomas Mondain-Monval <thoval@ceh.ac.uk>
2023-10-23 17:18:36 +0100

    finalising code to simulate a species and sampled in a biassed way

--- scripts/processing/simulate_species.R
+++ scripts/processing/simulate_species.R
@@ -1,46 +1,65 @@
 
-
 ## simulating species
 
 library(terra)
 library(sf)
 
-list.files('data',
-           full.names = TRUE)
+# function to sample a raster
+sample_rast <- function(samplerast, n_vals) {
+  
+  # function to convert between 0-1
+  scale_values <- function(x){(x-min(x))/(max(x)-min(x))}
+  
+  # convert to raster
+  sample_dat <- as.data.frame(samplerast, xy = TRUE)
+  
+  # get an index for sampling - probability final column
+  row_ind <- sample(1:nrow(sample_dat), size = n_vals, prob = scale_values(sample_dat[,3]))
+  
+  # return sampled rows
+  samples <- sample_dat[row_ind,]
+  
+  return(samples)
+  
+}
 
-butterflyeff <- rast('data/butterfly_1km_effort_layer.grd')
-plot(log(butterflyeff))
-
+# load environmental data
 envdat <- rast("data/envdata_1km_no_corr_noNA.grd")
 plot(envdat[[1]])
-envdat
 names(envdat)
 
-uk <- st_read('data/uk_counties2023_ONS/CTY_MAY_2023_EN_BFC.shp')
-uk
-lancs <- st_geometry(uk[uk$CTY23NM == 'Lancashire',])
-plot(lancs)
 
-# crop layers
-env_lancs <- terra::crop(envdat, vect(lancs), mask = TRUE)
-plot(env_lancs)
+##### simulate true distribution of a single species
+# combination of precipitation seasonailty, elevation and broadleaf woodland
+species_layer <- envdat[["PrecipSeasonality"]]*envdat[["elevation_UK"]]*envdat[["broad_wood"]]
+plot(species_layer)
 
-buttlancs <- terra::crop(butterflyeff, vect(lancs), mask = TRUE)
-plot(log(buttlancs))
+# sample n locs to get true species' distribution
+true_species_locs <- sample_rast(species_layer, n_vals = 1000)[,c('x','y')]
 
-beff <- as.data.frame(butterflyeff, xy = TRUE)
-head(beff)
+# convert to sf object
+true_species_dist <- st_as_sf(true_species_locs, coords = c("x", "y"), crs = 27700)
+plot(st_geometry(true_species_dist), pch = 4, add = TRUE) ## species distribution
 
-row_ind <- sample(1:nrow(beff), size = 1000, prob = beff$butterfly_1km_effort)
 
-samples <- beff[row_ind,]
+#### simulate biassed sampling of the species
+# sample according to suburban layer - biassed sampling
+true_species_suburban <- extract(envdat[['suburban']], true_species_dist)
 
-recsuk <- st_as_sf(samples, coords = c("x", "y"), crs = 27700)
-plot(log(butterflyeff))
-plot(recsuk, add = TRUE, pch = 20)
+# get biassed sampling of true species distribution
+samp_spp_ind <- sample(1:nrow(true_species_locs), size = 350, prob = true_species_suburban[,2])
+sampled_species_distrib <- true_species_locs[samp_spp_ind, c('x', 'y')]
 
-records_lancs <- mask(recsuk, vect(lancs))
+points(x = sampled_species_distrib$x, y = sampled_species_distrib$y, 
+       pch = 4, col = "red")
 
-plot(log(buttlancs))
-plot(recsuk, add = TRUE, pch = 20)
+head(sampled_species_distrib)
 
+#### write out data
+# true species distribution
+write.csv(true_species_locs, 
+          file = 'outputs/simulated_data/true_species_distribution.csv')
+
+# sampled species distribution
+write.csv(sampled_species_distrib, 
+          file = 'outputs/simulated_data/sampled_species_distrib.csv')
