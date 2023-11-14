386b0a7bfecf23e329a95576459d5cb2691429ec
Thomas Mondain-Monval <thoval@ceh.ac.uk>
2023-11-14 16:23:32 +0000

    Changed the batch sampling section to a fucntion and moved it to the functions script

--- scripts/functions/as_functions.R
+++ scripts/functions/as_functions.R
@@ -129,3 +129,137 @@ model_predict <- function(datato_model, environ_data) {
 }
 
 
+
+# Function to run rounds of adaptive sampling
+adaptive_sampling_rounds <- function(modelbased_crit_sf,
+                                     initial_species_records,
+                                     distance_apart,
+                                     nbatches,
+                                     total_number_locs) {
+  
+  # convert initial points to sf object
+  initial_species_records_sf <- st_as_sf(initial_species_records[,c("x", "y")], 
+                                         coords = c("x", "y"), 
+                                         crs = 27700) 
+  
+  # output data frames
+  new_locs_out <- data.frame() # includes new locs where species wasn't
+  new_species_observations <- data.frame() # only locations where species was seen
+  modeluncertainty_out <- data_frame() # storing model uncertainty
+  
+  # print statement
+  print(paste("Sampling", round(total_number_locs/nbatches), "per batch. Number of batches =", nbatches))
+  
+  ## start of for loop
+  for(batch_num in 1:nbatches) {
+    
+    print(paste("Batch", batch_num))
+    
+    #### remove sampling occasions close to existing records (from adaptive sampling)
+    # ideally would include the old locations too but would take AGES to run.
+    if(batch_num>1){
+      print(paste("Removing potential sampling occasions within", distance_apart, "m of previous adaptively sampled locations"))
+      
+      # convert new records to sf
+      new_species_observations_sf <- st_as_sf(new_locs_out[,c("x", "y")], # use dataframe with all selected locations in
+                                              coords = c("x", "y"), 
+                                              crs = 27700)
+      
+      # find all points that are in the same location - that intersect
+      points_intersect <- unlist(st_intersects(new_species_observations_sf, 
+                                               modelbased_crit_sf,
+                                               dist = distance_apart))
+      
+      # find all points within x distance of highest uncert
+      points_within_dist <- unlist(st_is_within_distance(new_species_observations_sf, 
+                                                         modelbased_crit_sf,
+                                                         dist = distance_apart))
+      
+      if(length(c(points_within_dist, points_intersect))>0){
+        # remove those points from the uncertainty dataframe 
+        modelbased_crit_sf <- modelbased_crit_sf[-c(points_within_dist, points_intersect),]
+      }
+      
+      # # bind to initial points
+      # initial_species_records_sf <- rbind(initial_species_records_sf, new_species_observations_sf)
+    }
+    
+    
+    #### Get new sampling occasions
+    print(paste("Getting sampling occasions"))
+    {
+      
+      ## sample locations optimally from model uncertainty
+      batch <- optimise_sampling_uncertainty(uncertainty_dataframe = modelbased_crit_sf,
+                                             number_locactions = round(total_number_locs/nbatches))
+      
+      ## check whether they find new records - Go out and sample observations
+      new_locs <- check_distrib(new_records = terra::vect(batch), 
+                                true_species_distrib = species_distrib_rast)
+      
+      ## store new species observations
+      new_locs$batch <- batch_num
+      
+      # store new locs including locations where species wasn't
+      new_locs_out <- rbind(new_locs_out, 
+                            new_locs)
+      
+      # # store new sampling occasions of only locs where species was seen
+      # new_species_observations <- rbind(new_species_observations, 
+      #                                   new_locs[new_locs$observations == 1,])
+    }
+    
+    #### prepare for modelling
+    print(paste("Preparing for modelling"))
+    {
+      ## bind to initial records
+      all_records <- rbind(initial_species_records[,1:2], 
+                           new_locs_out[new_locs_out$observations==1, c("x", "y")]) ## should we add batch to this so that we can account for it in the modelling?
+      
+      ## remove duplicates for modelling
+      all_records <- all_records[!duplicated(all_records),]
+      
+      ## add observations column
+      all_records$observations <- 1
+      
+      ## generate pseudoabsences from unvisited locations
+      absence_cells <- find_empirical_absences(all_records[,1:2], envdat[[1]])
+      
+      ## bind presences and pseudos together
+      modelling_df <- rbind(all_records, absence_cells)
+      
+      ## extract environmental data
+      modelling_df <- cbind(modelling_df, terra::extract(envdat, modelling_df[,1:2], 
+                                                         xy = FALSE, method = "simple"))
+    }
+    
+    ## modelling - gams
+    modelpredictions <- model_predict(datato_model = modelling_df, environ_data = envdat)
+    
+    #### extract uncertainty
+    print(paste("Getting uncertainty"))
+    {
+      # get standard error
+      modeluncertainty <- modelpredictions[,c("x", "y", "se.fit")]
+      
+      # add batch number
+      modeluncertainty$batch <- batch_num
+      
+      # store
+      modeluncertainty_out <- rbind(modeluncertainty_out, modeluncertainty)
+      
+      # convert to sf
+      modelbased_crit_sf <- st_as_sf(modeluncertainty, coords = c("x", "y"), crs = 27700)
+      
+      # order the uncertainty layer according to decreasing uncertainty - se.fit
+      # this is the layer that is updated for the next round of sampling
+      modelbased_crit_sf <- modelbased_crit_sf[order(modelbased_crit_sf$se.fit,
+                                                     decreasing = TRUE), ]
+    }
+    
+  }
+  
+  return(list(all_adaptive_locations = new_locs_out,
+              model_uncertainty = modeluncertainty_out))
+  
+}
